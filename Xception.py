1dcc806db4268cd25f35281892a91b59cc795e6f
alon <alongloberman@gmail.com>
2018-03-28 22:45:23 +0300

    updating laptop adding Xception adding Tensor_Generator because im retarded

--- Xception.py
+++ Xception.py
@@ -10,9 +10,9 @@ base_model = Xception(weights='imagenet', include_top=False)
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 # let's add a fully-connected layer
-x = Dense(64, activation='relu')(x)
+x = Dense(128, activation='relu')(x)
 # and a logistic layer -- let's say we have 3 classes
-predictions = Dense(5, activation='softmax')(x)
+predictions = Dense(3, activation='softmax')(x)
 
 # this is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
@@ -35,9 +35,9 @@ for i, layer in enumerate(base_model.layers):
 
 # we chose to train the top 2 inception blocks, i.e. we will freeze
 # the first 249 layers and unfreeze the rest:
-for layer in model.layers[:131]:
+for layer in model.layers[:120]:
    layer.trainable = False
-for layer in model.layers[131:]:
+for layer in model.layers[120:]:
    layer.trainable = True
 
 # we need to recompile the model for these modifications to take effect
@@ -47,11 +47,11 @@ model.compile(adam_optimizer, loss='categorical_crossentropy')
 
 # import DataGenerator
 from modules.Tensor_generator import initiate_generators
-train_generator, validation_generator = initiate_generators(batch_size=10)
+train_generator, validation_generator = initiate_generators(batchSize=20)
 
 model.fit_generator(
         train_generator,
         steps_per_epoch=1000,
-        epochs=50,
+        epochs=100,
         validation_data=validation_generator,
         validation_steps=2000)
